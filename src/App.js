2b768239b5a6cab79e2b0ae7a4af1474f9342ef8
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-10-26 11:48:33 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu, Switch } from 'antd';
-
+import DataTable from 'react-data-table-component';
 
 function getItem(label, key, icon, children, type) {
   return {
@@ -40,41 +40,29 @@ const items = [
   ]),
 ];
 
+
+
 const App = () => {
-  const [theme, setTheme] = useState('dark');
   const [current, setCurrent] = useState('1');
-  const changeTheme = (value) => {
-    setTheme(value ? 'dark' : 'light');
-  };
+
   const onClick = (e) => {
     console.log('click ', e);
     setCurrent(e.key);
   };
+
   return (
-    <>
-      <Switch
-        checked={theme === 'dark'}
-        onChange={changeTheme}
-        checkedChildren="Dark"
-        unCheckedChildren="Light"
-      />
-      <br />
-      <br />
-      <Menu
-        theme={theme}
-        onClick={onClick}
-        style={{
-          width: 256,
-        }}
-        defaultOpenKeys={['sub1']}
-        selectedKeys={[current]}
-        mode="inline"
-        items={items}
-      />
-    </>
+    <Menu
+      theme="dark" 
+      onClick={onClick}
+      style={{
+        width: 256,
+      }}
+      defaultOpenKeys={['sub1']}
+      selectedKeys={[current]}
+      mode="inline"
+      items={items}
+    />
   );
 };
-
-
 
 export default App;
