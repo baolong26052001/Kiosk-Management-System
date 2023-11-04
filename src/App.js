9fd6b24fc382467ddd545044d1c09e015c97b153
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-11-04 21:54:22 +0700

    update sidebar info

--- src/App.js
+++ src/App.js
@@ -20,24 +20,25 @@ const items = [
     getItem('User Group', '2'),
     getItem('Kiosk Setup', '3'),
     getItem('Kiosk Hardware', '4'),
-    getItem('Access Rule', '5'),
+    //getItem('Access Rule', '5'),
     getItem('Station', '6'),
     getItem('Video Slideshow Setup', '7'),
   ]),
   getItem('Transaction', 'sub2', <AppstoreOutlined />, [
     getItem('Account', '8'),
-    getItem('Loan Account', '9'),
-    getItem('Saving Account', '10'),
-    getItem('Statement', '11'),
+    getItem('Loan Transaction', '9'),
+    getItem('Saving Transaction', '10'),
+    getItem('Loan Statement', '11'),
+    getItem('Saving Statement', '12'),
   ]),
   getItem('Logs', 'sub3', <SettingOutlined />, [
-    getItem('Transaction Logs', '12'),
-    getItem('Activity Logs', '13'),
-    getItem('Notification Logs', '14'),
-    getItem('Audit', '15'),
+    getItem('Transaction Logs', '13'),
+    getItem('Activity Logs', '14'),
+    getItem('Notification Logs', '15'),
+    getItem('Audit', '16'),
   ]),
   getItem('Report', 'sub4', <SettingOutlined />, [
-    getItem('Kiosk Up/Down Time', '16'),
+    getItem('Kiosk Health', '17'),
   ]),
 ];
 
@@ -79,6 +80,13 @@ const columns = [
     sortable: true,
   },
 ];
+
+const Navbar = () => (
+  <div className="navbar">
+    {/* Add your navbar content here */}
+    <h1>Navbar</h1>
+  </div>
+);
 
 const App = () => {
   const [current, setCurrent] = useState('1');
