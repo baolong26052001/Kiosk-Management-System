483c7bfdb62de866b86adb909e882d5914b01afa
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-11-09 11:57:55 +0700

    thêm chữ ở trong 4 box

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -17,28 +17,36 @@ const Dashboard = () => {
         <div class="allcard">
             <div class="card1">
                 <div class="container">
-                    
+                    <p class="kiosktext">Total kiosk</p>
+                    <h2 class="kiosktext numberkiosk">100</h2>
+                    <p class="kiosktext">Last week analytics</p>
                 </div>
             </div>
             <div class="card2">
                 <div class="container">
-                    
+                    <p class="kiosktext">Total kiosk</p>
+                    <h2 class="kiosktext numberkiosk">100</h2>
+                    <p class="kiosktext">Last week analytics</p>
                 </div>
             </div>
             <div class="card3">
                 <div class="container">
-                    
+                    <p class="kiosktext">Total kiosk</p>
+                    <h2 class="kiosktext numberkiosk">100</h2>
+                    <p class="kiosktext">Last week analytics</p>
                 </div>
             </div>
             <div class="card4">
                 <div class="container">
-                    
+                    <p class="kiosktext">Total kiosk</p>
+                    <h2 class="kiosktext numberkiosk">100</h2>
+                    <p class="kiosktext">Last week analytics</p>
                 </div>
             </div>
         </div>
         
         <div class="bigcard">
-            
+
         </div>
 
     </div>
