d5248e787f80c438bb377f191bccf5ae950dfb66
baolong26052001 <1959015@itec.hcmus.edu.vn>
2023-11-10 15:23:30 +0700

    xong onclick

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -46,7 +46,9 @@ const items = [
 // submenu keys of first level
 const rootSubmenuKeys = ['sub1', 'sub2', 'sub3', 'sub4'];
 const Sidebar = () => {
+
   const [openKeys, setOpenKeys] = useState(['17']);
+
   const onOpenChange = (keys) => {
     const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
     if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
@@ -55,6 +57,69 @@ const Sidebar = () => {
       setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
     }
   };
+
+  const handleMenuClick = (e) => {
+    const clickedKey = e.key;
+    
+    // Your logic to handle the clicked menu item
+    switch (clickedKey) {
+      case '17':
+        window.location.href = '/';
+        break;
+      case '1':
+        window.location.href = '/users';
+        break;
+      case '2':
+        window.location.href = '/usersgroup';
+        break;
+      case '3':
+        window.location.href = '/kiosksetup';
+        break;
+      case '4':
+        window.location.href = '/kioskhardware';
+        break;
+      case '5':
+        window.location.href = '/station';
+        break;
+      case '6':
+        window.location.href = '/slideshow';
+        break;
+      case '7':
+        window.location.href = '/account';
+        break;
+      case '8':
+        window.location.href = '/loantransaction';
+        break;
+      case '9':
+        window.location.href = '/savingtransaction';
+        break;
+      case '10':
+        window.location.href = '/loanstatement';
+        break;
+      case '11':
+        window.location.href = '/savingstatement';
+        break;
+      case '12':
+        window.location.href = '/transactionlogs';
+        break;
+      case '13':
+        window.location.href = '/activitylogs';
+        break;
+      case '14':
+        window.location.href = '/notificationlogs';
+        break;
+      case '15':
+        window.location.href = '/audit';
+        break;
+      case '16':
+        window.location.href = '/kioskhealth';
+        break;
+      default:
+        console.log('Key not handled:', clickedKey);
+        break;
+    }
+  };
+
   return (
     <Menu
       theme="dark"
@@ -71,8 +136,30 @@ const Sidebar = () => {
       }}
       defaultSelectedKeys={['17']}
       items={items}
-    />
+      onClick={handleMenuClick}
+    >
+      {items.map((item) => {
+        if (item.children) {
+          return (
+            <Menu.SubMenu key={item.key} title={item.label} icon={item.icon}>
+              {item.children.map((subItem) => (
+                <Menu.Item key={subItem.key} icon={subItem.icon}>
+                  <Link to={`/route/${subItem.key}`}>{subItem.label}</Link>
+                </Menu.Item>
+              ))}
+            </Menu.SubMenu>
+          );
+        } else {
+          return (
+            <Menu.Item key={item.key} icon={item.icon}>
+              <Link to={`/route/${item.key}`}>{item.label}</Link>
+            </Menu.Item>
+          );
+        }
+      })}
+    </Menu>
   );
+
 };
 
-export default Sidebar
+export default Sidebar;
