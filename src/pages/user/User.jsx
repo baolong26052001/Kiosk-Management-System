36d7e750ce3a635ff9c667ebb0866d676f84e023
coftbred <nkluyen123@gmail.com>
2023-11-23 16:28:47 +0700

    Update table

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,42 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import {EyeOutlined, PlusOutlined, DeleteOutlined } from '@ant-design/icons';
 
 //import Sidebar from '../components/sidebar/Sidebar';
 import { render } from '@testing-library/react';
 
+// import components from MUI
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Button, Box } from '@mui/material';
+// import { useHistory } from 'react-router-dom'; // Import useHistory from React Router
 
 //import css
 import './User.css';
+import { Flex } from 'antd';
+
+
+
+const ViewButton = ({ rowId, label, onClick }) => {
+  const handleClick = (event) => {
+    event.stopPropagation(); // Stop the click event from propagating to the parent DataGrid row
+    onClick(rowId);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+      <Button size="small" variant="contained" onClick={handleClick}>
+        {label}
+      </Button>
+    </Box>
+  );
+};
+
+const EditButton = ({ rowId, label, onClick }) => {
+  const handleClick = (event) => {
+    event.stopPropagation(); // Stop the click event from propagating to the parent DataGrid row
+    onClick(rowId);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+      <Button size="small"  variant="contained" color="warning" onClick={handleClick}>
+        {label}
+      </Button>
+    </Box>
+  );
+};
 
 const columns = [
-  { field: 'id', headerName: 'User ID', flex: 1 },
-  { field: 'userName', headerName: 'User Name', flex: 2 },
-  { field: 'email', headerName: 'Email', flex: 2 },
+  {
+    field: 'viewButton',
+    headerName: '',
+    minWidth: 70,
+    sortable: false, // Disable sorting for this column
+    filterable: false, // Disable filtering for this column
+    renderCell: (params) => (
+        <ViewButton
+        rowId={params.row.id}
+        label="View"
+        onClick={handleButtonClick}
+      />
+    ),
+  },
+  {
+    field: 'editButton',
+    headerName: '',
+    minWidth: 70, 
+    sortable: false, // Disable sorting for this column
+    filterable: false, // Disable filtering for this column
+    renderCell: (params) => (
+        <EditButton
+        rowId={params.row.id}
+        label="Edit"
+        onClick={handleButtonClick}
+      />
+    ),
+  },
+  { field: 'id', headerName: 'User ID', 
+    minWidth: 100,
+},
+  { field: 'userName', headerName: 'User Name', 
+    minWidth: 150, 
+  },
+  { field: 'email', headerName: 'Email', 
+    minWidth: 200,
+  },
   {
     field: 'userGroup',
     headerName: 'User Group',
     sortable: false,
-  //   type: 'number',
-      flex: 2,
+    minWidth: 150,
   },
   {
     field: 'active',
     headerName: 'Active',
     sortable: false,
-    flex: 1,
+    minWidth: 100, 
   },
   {
-      field: 'lastLogin',
-      headerName: 'Last Login',
-      sortable: false,
-      flex: 2,
+    field: 'lastLogin',
+    headerName: 'Last Login',
+    sortable: false,
+    minWidth: 200,
   },
   {
-      field: 'daysDormant',
-      headerName: 'Total Days Dormant',
-      sortable: false,
-      flex: 2,
+    field: 'daysDormant',
+    headerName: 'Total Days Dormant',
+    sortable: false,
+    minWidth: 200,
    },
 ];
 
@@ -51,11 +119,14 @@ const rows = [
   { id: 8, userName: 'Tessta', email: 'tessta@gmail.com', userGroup: 'Administration', active: 'Yes', lastLogin: '24-12-2023 14:32:43', daysDormant: '10' },
   { id: 9, userName: 'Carena', email: 'carena@gmail.com', userGroup: 'Administration', active: 'Yes', lastLogin: '24-12-2023 14:32:43', daysDormant: '10' },
   { id: 10, userName: 'Wals', email: 'wals@gmail.com', userGroup: 'Administration', active: 'Yes', lastLogin: '24-12-2023 14:32:43', daysDormant: '10' },
-
-
 ];
 
-const KioskHardware = () => {
+const handleButtonClick = (id) => {
+  // Handle button click, e.g., navigate to another page
+  console.log(`Button clicked for row with ID: ${id}`);
+};
+
+const User = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedRows, setSelectedRows] = useState([]);
     const [selectAllChecked, setSelectAllChecked] = useState(false);
@@ -67,9 +138,12 @@ const KioskHardware = () => {
         setSearchTerm(searchTermButton);
     };
 
+    // const history = useHistory();
 
-
-
+    // const handleButtonClick = (id) => {
+    //   // Navigate to another page using React Router
+    //   history.push(``);
+    // };
 
   return (
     
@@ -79,30 +153,23 @@ const KioskHardware = () => {
             <h1 className="h1-dashboard">User</h1>
         </div>
             <div className="bigcarddashboard">
-                <div className="statusandimage">
-                    
-                </div>
-                
                 <div className="searchdivuser">
-
-                
                     <input onChange={(event) => setSearchTermButton(event.target.value)} placeholder="  Search..." type="text" id="kioskID myInput" name="kioskID" class="searchbar"></input>
-                    
                     <input onClick={handleSearchButton} type="button" value="Search" className="button button-search"></input>
                 </div>
 
                 
                 <div className='Table' style={{ height: 400, width: '100%'}}>
                     <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
-                        },
-                        }}
-                        pageSizeOptions={[5, 10]}
-                        checkboxSelection
+                      rows={rows}
+                      columns={columns}
+                      initialState={{
+                      pagination: {
+                          paginationModel: { page: 0, pageSize: 5 },
+                      },
+                      }}
+                      pageSizeOptions={[5, 10, 25, 50]}
+                      checkboxSelection
                     />
                 </div>
             </div>
@@ -110,7 +177,8 @@ const KioskHardware = () => {
         
     </div>
     
+    
   )
 }
 
-export default KioskHardware;
+export default User;
--- src/pages/usergroup/Usergroup.jsx
+++ src/pages/usergroup/Usergroup.jsx
@@ -1,384 +1,161 @@
 import React, { useState, useEffect } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu, Switch } from 'antd';
-import DataTable from 'react-data-table-component';
-import Accordion from 'react-bootstrap/Accordion';
+
 //import Sidebar from '../components/sidebar/Sidebar';
 import { render } from '@testing-library/react';
-import { Col, Divider, Row, Button, Form, Input } from 'antd';
-
-//import material UI
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
-//import material UI
 
 
 //import css
 import './Usergroup.css';
-//import css
 
+// import components from MUI
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Button, Box } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-// name of selection
-const names = [
-  'Administrator',
-  'Support',
-  'Manager',
-];
-// name of selection
+function createData(id, groupName, dateModified, dateCreated, isActive) {
+  return {id, groupName, dateModified, dateCreated, isActive };
+}
 
-const activeNames = [
-  'Yes',
-  'No',
-];
+const ViewButton = ({ rowId, label, onClick }) => {
+  const handleClick = (event) => {
+    event.stopPropagation(); // Stop the click event from propagating to the parent DataGrid row
+    onClick(rowId);
+  };
 
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+      <Button size="small" variant="contained" color="error" onClick={handleClick}>
+        {label}
+      </Button>
+    </Box>
+  );
 };
+
+const EditButton = ({ rowId, label, onClick }) => {
+  const handleClick = (event) => {
+    event.stopPropagation(); // Stop the click event from propagating to the parent DataGrid row
+    onClick(rowId);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+      <Button size="small"  variant="contained" color="warning" onClick={handleClick}>
+        {label}
+      </Button>
+    </Box>
+  );
+};
+
+
+
+const columns = [
+  {
+    field: 'permissionButton',
+    headerName: '',
+    minWidth: 150,
+    sortable: false, // Disable sorting for this column
+    filterable: false, // Disable filtering for this column
+    renderCell: (params) => (
+        <ViewButton
+        rowId={params.row.id}
+        label="Permission"
+        onClick={handleButtonClick}
+      />
+    ),
+  },
+  {
+    field: 'editButton',
+    headerName: '',
+    minWidth: 100,
+    sortable: false, // Disable sorting for this column
+    filterable: false, // Disable filtering for this column
+    renderCell: (params) => (
+        <EditButton
+        rowId={params.row.id}
+        label="Edit"
+        onClick={handleButtonClick}
+      />
+    ),
+  },
+  { field: 'id', headerName: 'Group ID', flex: 1, },
+  { field: 'groupName', headerName: 'Group Name', flex: 2, },
+  { field: 'dateModified', headerName: 'Date Modified', flex: 2,},
+  {
+    field: 'dateCreated',
+    headerName: 'Date Created',
+    sortable: false,
+    flex: 2,
+  },
+  {
+    field: 'isActive',
+    headerName: 'Is Active',
+    sortable: false,
+    flex: 1,
+  },
+];
+
+const rows = [
+  createData(1, 'Administration', '24-12-2023 12:00:00', '19-11-2023 10:23:11', 'Yes'),
+  createData(2, 'Support', '24-12-2023 12:00:00', '19-11-2023 10:23:11', 'Yes'),
+  createData(3, 'Mornitor', '24-12-2023 12:00:00', '19-11-2023 10:23:11', 'Yes'),
+];
+
+
+const handleButtonClick = (id) => {
+  // Handle button click, e.g., navigate to another page
+  console.log(`Button clicked for row with ID: ${id}`);
+};
+
 
 const Usergroup = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedRows, setSelectedRows] = useState([]);
     const [selectAllChecked, setSelectAllChecked] = useState(false);
     const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+
     const [searchTermButton, setSearchTermButton] = useState('');
-    const [personName, setPersonName] = React.useState([]);
-    const [activeName, setActiveName] = React.useState([]);
-
-    const handleChange = (event) => {
-      const {
-        target: { value },
-      } = event;
-      setPersonName(
-        // On autofill we get a stringified value.
-        typeof value === 'string' ? value.split(',') : value,
-      );
-      
-    };
-
-    const handleActiveChange = (event) => {
-      const {
-        target: { value },
-      } = event;
-      setActiveName(
-        typeof value === 'string' ? value.split(',') : value,
-      );
-    };
 
     const handleSearchButton = () => {
         setSearchTerm(searchTermButton);
     };
 
-    const renderSortIcon = (columnKey) => {
-        if (sortConfig && sortConfig.key === columnKey) {
-          return sortConfig.direction === 'ascending' ? <span class="arrow">&#9660;</span> : <span class="arrow">&#9650;</span>;
-        }
-        return sortConfig.direction === 'ascending' ? <span class="arrow">&#9660;</span> : <span class="arrow">&#9650;</span>;;
-    };
-      
-
-    const handleCheckboxChange = (event, item) => {
-      const { checked } = event.target;
-  
-      if (event.target.name === 'selectAll') {
-        // Handle "Select All" checkbox
-        setSelectAllChecked(checked);
-        setSelectedRows(checked ? data : []);
-      } else {
-        // Handle individual checkboxes
-        if (checked) {
-          // Add the selected item to the list
-          setSelectedRows((prevSelectedRows) => [...prevSelectedRows, item.userGroupID]);
-        } else {
-          // Remove the selected item from the list
-          setSelectedRows((prevSelectedRows) =>
-            prevSelectedRows.filter(
-              (selectedItem) => selectedItem !== item.userGroupID
-            )
-          );
-        }
-      }
-  };
-
-    const data = [
-
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 123, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 124, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 125, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-        
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 126, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 127, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-
-        { 
-            groupName: 'Administrator', 
-            userGroupID: 128, 
-            
-             
-            active: 'Yes', 
-            dateModified: '24-10-2023', 
-            dateCreated: '24-01-2023',
-            
-        },
-
-        
-      ];
-      
-      const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-      };
-      
-      const handleSort = (key) => {
-        let direction = 'ascending';
-        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
-          direction = 'descending';
-        }
-        setSortConfig({key, direction});
-      };
-    
-      const sortData = (data) => {
-        if (sortConfig.key) {
-          const sortedData = [...data];
-          sortedData.sort((a, b) => {
-            const keyA = a[sortConfig.key];
-            const keyB = b[sortConfig.key];
-            if (keyA < keyB) return sortConfig.direction === 'ascending' ? -1 : 1;
-            if (keyA > keyB) return sortConfig.direction === 'ascending' ? 1 : -1;
-            return 0;
-          });
-          return sortedData;
-        }
-        return data;
-      };
-
-      const filteredData = sortData(
-        data.filter((item) =>
-          Object.values(item).some(
-            (value) =>
-              value &&
-              value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-          )
-        )
-      );
 
   return (
     
-    <div class="content"> 
+    <div className="content"> 
 
-        <div class="admin-dashboard-text-div"> 
-            <p></p>
-            <h1 class="h1-dashboard">User Group</h1>
+        <div className="admin-dashboard-text-div"> 
+            <h1 className="h1-dashboard">User Group</h1>
         </div>
-   
-        
-            <div class="bigcarddashboard">
-                <div class="filterpart">
-                  
-                    
-                  
-                    
-                   
-                    
-                </div>
-                     <Row gutter={16}>
-                        <Col className="gutter-row" span={12}>
-                          <div>
-                            
-                            <FormControl sx={{ m: 1, width: 300 }}>
-                              <InputLabel id="group-name-label">Group Name</InputLabel>
-                              <Select
-                                labelId="group-name-label"
-                                id="group-name"
-                                multiple
-                                value={personName}
-                                onChange={handleChange}
-                                input={<OutlinedInput label="Group Name" />}
-                                renderValue={(selected) => selected.join(', ')}
-                                MenuProps={MenuProps}
-                              >
-                                {names.map((name) => (
-                                  <MenuItem key={name} value={name}>
-                                    <Checkbox checked={personName.indexOf(name) > -1} />
-                                    <ListItemText primary={name} />
-                                  </MenuItem>
-                                ))}
-                              </Select>
-                            </FormControl>
-                          </div>
-                        </Col>
+        <div className="bigcarddashboard">
 
-                        <Col className="gutter-row" span={12}>
-                          <div>
-                            
-                            <FormControl sx={{ m: 1, width: 300 }}>
-                              <InputLabel id="name-label">Active</InputLabel>
-                              <Select
-                                labelId="name-label"
-                                id="name"
-                                
-                                value={activeName}
-                                onChange={handleActiveChange}
-                                input={<OutlinedInput label="Active" />}
-                                renderValue={(selected) => selected.join(', ')}
-                                MenuProps={MenuProps}
-                              >
-                                {activeNames.map((name) => (
-                                  <MenuItem key={name} value={name}>
-                                    <Checkbox checked={activeName.indexOf(name) > -1} />
-                                    <ListItemText primary={name} />
-                                  </MenuItem>
-                                ))}
-                              </Select>
-                            </FormControl>
-                          </div>
-                        </Col>
-                        
-                      </Row>
+          <div className='Filter'>
+          
+          </div>
 
-                    
+          <div className="searchdivuser">
+              <input onChange={(event) => setSearchTermButton(event.target.value)} placeholder="  Search..." type="text" id="kioskID myInput" name="kioskID" class="searchbar"></input>
+              <input onClick={handleSearchButton} type="button" value="Search" className="button button-search"></input>
+          </div>
 
-                <div class="searchdiv">
-
-                
-                    <input onChange={(event) => setSearchTermButton(event.target.value)} placeholder="  Search package ID..." type="text" id="userGroupID myInput" name="userGroupID" class="searchbar"></input>
-                    
-                    <input onClick={handleSearchButton} type="button" value="Search" class="button button-search"></input>
-                </div>
-
-                <div class="tablediv">
-
-                
-                <table id="myTable">
-                    <thead>
-                        <tr>
-                        <th>
-                            <input
-                            type="checkbox"
-                            name="selectAll"
-                            checked={selectAllChecked}
-                            onChange={(event) => handleCheckboxChange(event, null)}
-                            />
-                        </th>
-                        <th></th>
-                        <th></th>
-                        <th onClick={() => handleSort('userGroupID')}>UserGroup ID {renderSortIcon('userGroupID')}</th>
-                        <th onClick={() => handleSort('groupName')}>Group Name {renderSortIcon('groupName')}</th>
-                        
-                        <th onClick={() => handleSort('active')}>Active {renderSortIcon('active')}</th>
-                        <th onClick={() => handleSort('dateModified')}>Date Modified {renderSortIcon('dateModified')}</th>
-                        <th onClick={() => handleSort('dateCreated')}>Date Created {renderSortIcon('dateCreated')}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredData.map((item, index) => (
-                        <tr key={index}>
-                          <td>
-                                <input
-                                type="checkbox"
-                                checked={selectAllChecked || selectedRows.includes(item.userGroupID)}
-                                onChange={(event) => handleCheckboxChange(event, item)}
-                                />
-                            </td>
-                            <td>
-                            <button class="edit-button">Edit</button>
-                            </td>
-                            <td>
-                            <button class="permission-button">Permission</button>
-                            </td>
-                            <td>{item.userGroupID}</td>
-                            <td>{item.groupName}</td>
-                            
-                            <td>{item.active}</td>
-                            <td>{item.dateModified}</td>
-                            <td>{item.dateCreated}</td>
-                            
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-                
-                </div>
-                
-
-                <div class="pageselectdashboard">
-                    <p class="rowperpagedash">Rows per page</p>
-                    <select class="listoptiondash">
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                    </select>
-                </div>
-                <div class="add-and-delete-button">
-                  <button class="add-button button">Add</button>
-                  <button class="delete-button button">Delete</button>
-                </div>
-                
-                
-            
-            </div>
-
-        
+          <div className='Table' style={{ height: 400, width: 'auto'}}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                      paginationModel: { page: 0, pageSize: 5 },
+                  },
+                  }}
+                  pageSizeOptions={[5, 10, 25, 50]}
+                  checkboxSelection
+                >
+              </DataGrid>
+          </div>
+        </div>
     </div>
     
   )
